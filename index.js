b418e046ec4e8ababf500c84cc6c4ceceaa9be4b
NathaliRodrigues <142436453+NathaliRodrigues@users.noreply.github.com>
2023-09-05 17:07:36 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,99 @@
+/*let condicao1 = true
+if (condicao1){
+    //como o valor da condicao é true,
+    // o codigo desse bloco é executado
+    console.log(`entrei no if 1!`)
+}
+
+let condicao2 = false
+ if (condicao2){
+    //como o valor da condicao é false,
+    // o codigo desse bloco NAO é executado
+    console.lol(`entrei no if 2!`)
+ }
+
+function recebeDoisnumeros (num1,num2){
+    if(num1 === num2){
+    return "suecesso";
+}
+}
+console.log(recebeDoisnumeros(Number(prompt("digite um numero")),Number(prompt("digite outro numero"))));
+
+function recebeTextoeNumero(nome,idade){
+    if (idade >= 18){
+        return nome + " voce pode dirigir";
+    }
+}
+
+console.log(recebeTextoeNumero(prompt("Qual seu nome?"), Number(prompt("Qual sua idade?"))));
+
+
+let condicao = false
+if(condicao){
+console.log(`entrei no if!`)
+}
+
+else{
+    //como o valor da condicao é false
+    // o codigo do bloco else será executado
+    console.log(`entrei  no else!`)
+}
+
+
+function recebeDoisnumeros (num1,num2){
+    if(num1 === num2){
+    return "sucesso";
+}
+}else{
+    return "nao teve sucesso"
+}
+console.log(recebeDoisnumeros(Number(prompt("digite um numero")),Number(prompt("digite outro numero"))));
+}
+
+function recebeDoisnumeros (num1,num2){
+    if(num1 === num2){
+        return "igual";
+    }else if(num1 > num2){
+        return"maior"
+    }else if(num1 < num2){
+        return"menor"
+    }else{
+        return "nao teve sucesso"
+    }
+}
+
+console.log(recebeDoisnumeros(Number(prompt("digite um numero")),Number(prompt("digite outro numero"))));
+
+let paisDeOrigem
+switch (paisDeOrigem){
+    case`Brasil`:
+    console.log(`brasileiro`)
+        break
+    case'EUA':
+    console.log(`norte americano`)
+        break
+        
+    case`Inglaterra`:
+    console.log(`ingles`)
+         break
+default:
+    console.log(`nacionalidade nao encontrada`)
+break
+}
+
+
+let pokemons = "Bulbassauro";
+switch(pokemons){
+    case 'Bulbassauro':
+        console.log('Planta e Veneno')
+    break
+    case 'Charmander':
+        console.log('Fogo')
+    break
+    case 'Squirtle':
+        console.log('água')
+    break
+default:
+    console.log('pokemon nao encontrado')
+}
+*/
